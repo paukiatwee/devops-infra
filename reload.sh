94d35e9acd4f31c5c2acf38a4c412d87a629f8e4
paukiatwee <paukiatwee@gmail.com>
2017-04-29 23:25:36 +0800

    better to reload known hosts

--- reload.sh
+++ reload.sh
@@ -2,8 +2,10 @@
 
 vagrant destroy --force && vagrant up --parallel && vagrant ssh k8s_master -c 'sudo systemctl restart network' && vagrant ssh worker01 -c 'sudo systemctl restart network' && vagrant ssh worker02 -c 'sudo systemctl restart network'
 
-rm -rf ~/.ssh/known_hosts
+ssh-keygen -R 192.168.88.88
+ssh-keygen -R 192.168.88.89
+ssh-keygen -R 192.168.88.90
 
-yes yes | ssh root@192.168.88.88
-yes yes | ssh root@192.168.88.89
-yes yes | ssh root@192.168.88.90
+ssh-keyscan 192.168.88.88 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.88.89 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.88.90 >> ~/.ssh/known_hosts
